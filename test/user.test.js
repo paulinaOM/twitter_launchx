773779087d3238e17b8f82cd0fc18ba221e80025
paulinaOM <17030622@itcelaya.edu.mx>
2022-04-20 14:03:43 -0500

    8. Unit testing for User class requirement #2

--- test/user.test.js
+++ test/user.test.js
@@ -2,13 +2,13 @@ const User = require('../app/models/user')
 
 describe("Unit test for User class", () =>{
     test("Create an user object", ()=>{
-        const user = new User(1, 'paulinaom', 'Paulina', 'Bio', 'dateCreated', 'lastUpdated');
+        const user = new User(1, 'paulinaom', 'Paulina', 'Bio');
         
         expect(user.id).toBe(1);
         expect(user.username).toBe('paulinaom');
         expect(user.name).toBe('Paulina');
         expect(user.bio).toBe('Bio');
-        expect(user.dateCreated).toBe('dateCreated');
-        expect(user.lastUpdated).toBe('lastUpdated');
+        expect(user.dateCreated).not.toBeUndefined();
+        expect(user.lastUpdated).not.toBeUndefined();
     });
 });
