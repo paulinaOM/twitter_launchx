c8f27757eccf5c104f8910dccea6cbb447b05037
paulinaOM <17030622@itcelaya.edu.mx>
2022-04-22 23:57:53 -0500

    21. UserService static method getInfo

--- app/services/UserService.js
+++ app/services/UserService.js
@@ -2,8 +2,12 @@ const User = require("../models/User");
 
 class UserService{
     static create(id, username, name){
-        
         return new User(id, username, name, "Bio")
+    }
+    static getInfo(user){
+        let attrList = new Array();
+        attrList.push(user.id, user.username, user.name, user.bio, user.dateCreated, user.lastUpdated);
+        return attrList;
     }
 }
 
