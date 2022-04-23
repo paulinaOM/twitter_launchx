a2bfa73a4c3dc83f27ec343603e9fb30969f2c73
paulinaOM <17030622@itcelaya.edu.mx>
2022-04-22 23:56:43 -0500

    20. Unit testing for UserService class spr2 req1: static method getInfo

--- test/services/UserService.test.js
+++ test/services/UserService.test.js
@@ -9,6 +9,19 @@ describe("Unit test for User class", () =>{
         expect(user.name).toBe('Paulina');
         expect(user.bio).not.toBeUndefined();
     });
+    test('Creating getInfo', ()=>{
+        const user = UserService.create(1, 'paulinaom', 'Paulina');
+        console.log(user)
+        const attr_list = UserService.getInfo(user);
+        console.log(attr_list)
+
+        expect(attr_list[0]).toBe(1);
+        expect(attr_list[1]).toBe('paulinaom');
+        expect(attr_list[2]).toBe('Paulina');
+        expect(attr_list[3]).toBe("Bio");
+        expect(attr_list[4]).not.toBeUndefined(); //dateCreated
+        expect(attr_list[5]).not.toBeUndefined(); //lastUpdated
+    })
 });
 
 
