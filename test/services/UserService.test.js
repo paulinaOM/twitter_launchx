31bc103d8c3a9c5e5e9a41e572e9c9ab58281147
paulinaOM <17030622@itcelaya.edu.mx>
2022-04-23 17:29:25 -0500

    22. Unit testing for UserService class spr2 req4: getAllUsernames

--- test/services/UserService.test.js
+++ test/services/UserService.test.js
@@ -11,16 +11,31 @@ describe("Unit test for User class", () =>{
     });
     test('Creating getInfo', ()=>{
         const user = UserService.create(1, 'paulinaom', 'Paulina');
-        console.log(user)
-        const attr_list = UserService.getInfo(user);
-        console.log(attr_list)
+        const attrList = UserService.getInfo(user);
 
-        expect(attr_list[0]).toBe(1);
-        expect(attr_list[1]).toBe('paulinaom');
-        expect(attr_list[2]).toBe('Paulina');
-        expect(attr_list[3]).toBe("Bio");
-        expect(attr_list[4]).not.toBeUndefined(); //dateCreated
-        expect(attr_list[5]).not.toBeUndefined(); //lastUpdated
+        expect(attrList[0]).toBe(1);
+        expect(attrList[1]).toBe('paulinaom');
+        expect(attrList[2]).toBe('Paulina');
+        expect(attrList[3]).toBe("Bio");
+        expect(attrList[4]).not.toBeUndefined(); //dateCreated
+        expect(attrList[5]).not.toBeUndefined(); //lastUpdated
+    })
+    test('Creating updateUsername static method', ()=>{
+        const user = UserService.create(1, 'paulinaom', 'Paulina');
+        UserService.updateUsername(user, "jesus");
+
+        expect(user.username).toBe("jesus");
+    })
+    test('Creating getAllUsernames static method', ()=>{
+        const user1 = UserService.create(1, 'paulinaom', 'Paulina');
+        const user2 = UserService.create(1, 'jesus', 'Jesus');
+        const user3 = UserService.create(1, 'jose', 'Jose');
+        const usernamesList = UserService.getAllUsernames([user1, user2, user3]);
+
+        expect(usernamesList[0]).toBe("paulinaom");
+        expect(usernamesList[1]).toBe("jesus");
+        expect(usernamesList[2]).toBe("jose");
+        
     })
 });
 
