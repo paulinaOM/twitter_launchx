7fc53933a2393df73c89af90a7684ff95e52341e
paulinaOM <17030622@itcelaya.edu.mx>
2022-04-22 22:43:41 -0500

    15. Unit testing for spr 2 req 1 static method

---
+++ test/models/user.test.js
@@ -0,0 +1,41 @@
+const User = require('../../app/models/user');
+const UserService = require('../../app/services/UserService');
+
+describe("Unit test for User class", () =>{
+    test("Create an user object", ()=>{
+        const user = new User(1, 'paulinaom', 'Paulina', 'Bio');
+        
+        expect(user.id).toBe(1);
+        expect(user.username).toBe('paulinaom');
+        expect(user.name).toBe('Paulina');
+        expect(user.bio).toBe('Bio');
+        expect(user.dateCreated).not.toBeUndefined();
+        expect(user.lastUpdated).not.toBeUndefined();
+    });
+    test('Create getters for User class', ()=>{
+        const user = new User(1, 'paulinaom', 'Paulina', 'Bio');
+
+        expect(user.getUsername).toBe('paulinaom');
+        expect(user.getName).toBe('Paulina');
+        expect(user.getBio).toBe('Bio');
+        expect(user.getDateCreated).not.toBeUndefined();
+        expect(user.getLastUpdated).not.toBeUndefined();
+    });
+    test('Creating setters for User class', ()=>{
+        const user = new User();
+
+        user.setUsername = 'paulinaom';
+        user.setBio = 'Bio';
+
+        expect(user.getUsername).toBe('paulinaom');
+        expect(user.getBio).toBe('Bio');
+    }); 
+    test('Creating UserService', ()=>{
+        const user = UserService.create(1, 'paulinaom', 'Paulina');
+
+        expect(user.id).toBe(1);
+        expect(user.username).toBe('paulinaom');
+        expect(user.name).toBe('Paulina');
+        expect(user.bio).not.toBeUndefined();
+    })
+});
