1b2803ced6bb75f655faecab20d29cdd36ac1156
paulinaOM <17030622@itcelaya.edu.mx>
2022-04-23 23:28:49 -0500

    23. Changing unit test description for UserService.test.js

--- app/services/UserService.js
+++ app/services/UserService.js
@@ -5,9 +5,18 @@ class UserService{
         return new User(id, username, name, "Bio")
     }
     static getInfo(user){
-        let attrList = new Array();
+        let attrList = [];
         attrList.push(user.id, user.username, user.name, user.bio, user.dateCreated, user.lastUpdated);
         return attrList;
+    }
+    static updateUsername(user, newUsername){
+        user.username = newUsername;
+        
+    }
+    static getAllUsernames(users){
+        let usernamesList = [];
+        users.forEach(user =>{ usernamesList.push(user.username)})
+        return usernamesList;
     }
 }
 
